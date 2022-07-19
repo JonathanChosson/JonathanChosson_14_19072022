3fbb15176cee60e34f1c18ae4fff183b0321363c
Jonathan Chosson <jonathan.chosson@gmail.com>
2022-07-19 15:01:27 +0200

    [REACT] create the base components :package:

---
+++ src/Features/employeeList.js
@@ -0,0 +1,87 @@
+import produce from 'immer'
+
+// import { selectLogin } from '../utils/selectors'
+
+
+
+const initialState = {
+    data: null,
+}
+
+const FETCHING = 'login/fetching'
+const RESOLVED = 'login/resolved'
+const REJECTED = 'login/rejected'
+const LOGINSTORAGE = 'login/storage'
+
+const loginAxios = () => ({ type: FETCHING })
+const loginResolved = (data) => ({ type: RESOLVED, payload: data })
+const loginRejected = (error) => ({ type: REJECTED, payload: error })
+export const loginStorage = (data) => ({ type: LOGINSTORAGE, payload: data })
+
+export async function axiosLogin(store, input, remember) {
+    const status = selectLogin(store.getState()).status
+    if (status === 'pending' || status === 'updating') {
+        return
+    }
+    store.dispatch(loginAxios())
+    try {
+        const response = await axios.post(
+            BASE_URL + '/api/v1/user/login',
+            input
+        )
+        const data = await response
+        if (remember) {
+            localStorage.setItem('JWT_Key', JSON.stringify(data.data))
+        }
+        store.dispatch(loginResolved(data.data))
+    } catch (error) {
+        store.dispatch(loginRejected(error.response.data))
+    }
+}
+
+export default function loginReducer(state = initialState, action) {
+    return produce(state, (draft) => {
+        switch (action.type) {
+            case FETCHING: {
+                if (draft.status === 'void') {
+                    draft.status = 'pending'
+                    return
+                }
+                if (draft.status === 'rejected') {
+                    draft.error = null
+                    draft.status = 'pending'
+                    return
+                }
+                if (draft.status === 'resolved') {
+                    draft.status = 'updating'
+                    return
+                }
+                return
+            }
+            case RESOLVED: {
+                if (draft.status === 'pending' || draft.status === 'updating') {
+                    draft.data = action.payload
+                    draft.status = 'resolved'
+                    return
+                }
+                return
+            }
+            case REJECTED: {
+                if (draft.status === 'pending' || draft.status === 'updating') {
+                    draft.error = action.payload
+                    draft.data = null
+                    draft.status = 'rejected'
+                    return
+                }
+                return
+            }
+            case LOGINSTORAGE: {
+                draft.data = action.payload
+                draft.status = 'resolved'
+                return
+            }
+            default:
+                return
+        }
+    })
+}
