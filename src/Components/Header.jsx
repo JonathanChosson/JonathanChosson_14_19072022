d722feba64ad9602f5ca9f3f9962885ba7eb7039
Jonathan Chosson <jonathan.chosson@gmail.com>
2022-07-19 15:29:36 +0200

    [JSX/CSS] Editing the Header Component :building_construction:

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import "../Style/Components/Header.css"
+
 
 const Header = () => {
     return (
-        <div>
-            
+        <div className='Header'>
+            <img src='.\logo_Wealth_Health.jpg' alt='HRnet' className='Header__logo'/>
+            <h1 className='Header__h1'>HRnet</h1>
         </div>
     );
 };
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,7 +3,7 @@ import Header from '../Components/Header';
 
 const Home = () => {
     return (
-        <div>
+        <div className='Home'>
             <Header />
         </div>
     );
