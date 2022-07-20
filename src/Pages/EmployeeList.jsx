bd70237bb549da63294400ea3c9ec91708603299
Jonathan Chosson <jonathan.chosson@gmail.com>
2022-07-20 16:02:39 +0200

    [JSX] Edition of listing employee plugin :arrow_down:

--- src/Pages/EmployeeList.jsx
+++ src/Pages/EmployeeList.jsx
@@ -1,13 +1,25 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 import Header from '../Components/Header'
 import Route from '../Components/Route'
 import '../Style/Pages/EmployeeList.css'
+import { selectEmployee } from '../Utils/selector'
+import DataTables from 'datatables-plugin-react'
+import { exampleLabels } from '../Data/exampleLabel.js'
 
 const EmployeeList = () => {
+    const employeeTable = useSelector(selectEmployee)
+    console.log(employeeTable.data)
+
     return (
         <div className="EmployeeList">
             <Header />
             <Route />
+            {employeeTable.data?.length >= 1 ? (
+                <DataTables labels={exampleLabels} data={employeeTable.data} />
+            ) : (
+                'Please create an employee to see their informations'
+            )}
         </div>
     )
 }
--- src/index.js
+++ src/index.js
@@ -1,18 +1,27 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
 import { Provider } from 'react-redux'
 import store from './Utils/store'
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(
+// const root = ReactDOM.createRoot(document.getElementById('root'))
+// root.render(
+//     <Provider store={store}>
+//         <React.StrictMode>
+//             <App />
+//         </React.StrictMode>
+//     </Provider>
+// )
+
+ReactDOM.render(
     <Provider store={store}>
         <React.StrictMode>
             <App />
         </React.StrictMode>
-    </Provider>
+    </Provider>,
+    document.getElementById('root')
 )
 
 // If you want to start measuring performance in your app, pass a function
